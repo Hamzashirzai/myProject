d7d25e56c9ccf36105bc28535bbbde8253ac257d
Hamaza <artwagerhs@gmail.com>
2015-10-29 14:16:09 +0100

    assignment

---
+++ AarhuswebDevcoopsql/Controllers/MySurfaceController.cs
@@ -0,0 +1,57 @@
+﻿using AarhuswebDevcoopsql.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using System.Net.Mail;
+using Umbraco.Web.Models;
+using Umbraco.Core.Models;
+
+
+namespace AarhuswebDevcoopsql.Controllers
+{
+   
+    public class MysurfaceController :SurfaceController
+
+    {
+        //
+        // GET: /ContactFormSurface/
+
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFromSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "Comment");
+
+            comment.SetValue("name", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("message", model.Message);
+
+            Services.ContentService.Save(comment);
+
+
+            MailMessage message = new MailMessage();
+            message.To.Add(model.Email);
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message + "\n my email is: " + model.Email;
+
+            using (SmtpClient smtp = new SmtpClient())
+
+                TempData["success"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
